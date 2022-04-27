5652e2eeca85ca3bad9a47a7374d44ad1491db6e
HumbertoRojasH <85846912+HumbertoRojasH@users.noreply.github.com>
2022-04-26 19:25:25 -0500

    Update mesh.py

--- Analysis with PMEF/mesh.py
+++ Analysis with PMEF/mesh.py
@@ -11,9 +11,3 @@ nodos = mesh.points
 connection = mesh.get_cells_type("hexahedron")
 cells = {"hexahedron": connection}
 meshio.write_points_cells("object.vtk", nodos, cells)
-
-# obj = pv.read("object.vtk")
-# surf = obj.extract_surface()
-# p = pv.Plotter()
-# p.add_mesh(surf,show_edges=True)
-# p.export_html('object.html')
